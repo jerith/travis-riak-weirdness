30de7cac6a8d97e32f29c8a221a889e745ddee52
Jeremy Thurgood <firxen@gmail.com>
2015-02-06 12:44:29 +0200

    Pom pom pom.

--- travis-riak-weirdness.sh
+++ travis-riak-weirdness.sh
@@ -12,7 +12,7 @@ cat $0
 # sudo riak stop
 # # sudo sed -i.bak 's/{vnode_vclocks, true}/{http_url_encoding, on}, {vnode_vclocks, true}/' /etc/riak/app.config
 # sudo riak start
-sleep 5
+sleep 30
 
 sudo riak-admin ring-status
 sudo riak-admin status
