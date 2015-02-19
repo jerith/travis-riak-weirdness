6240cedf7137435829483e683a904c0171a97071
Jeremy Thurgood <firxen@gmail.com>
2015-02-19 11:39:10 +0200

    Look at Riak configs.

--- travis-riak-weirdness.sh
+++ travis-riak-weirdness.sh
@@ -14,6 +14,7 @@ cat $0
 # sudo riak start
 sleep 30
 
+sudo riak version
 sudo riak-admin ring-status
 sudo riak-admin status
 
@@ -35,6 +36,11 @@ curl 'http://127.0.0.1:8098/riak/mybucket/foo%3Abaz'; echo ""
 curl 'http://127.0.0.1:8098/riak/mybucket/foo:baz'; echo ""
 curl 'http://localhost:8098/buckets/mybucket/index/field1_bin/val2'; echo ""
 
-echo -e "\nConfig:\n"
-
+echo -e "\napp.config:\n"
 cat /etc/riak/app.config
+
+echo -e "\nriak.conf:\n"
+cat /etc/riak/riak.conf
+
+echo -e "\nadvanced.config:\n"
+cat /etc/riak/advanced.config
