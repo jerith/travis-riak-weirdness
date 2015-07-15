3dba049633474bab8ec34666e90637cdf571ece4
Jeremy Thurgood <firxen@gmail.com>
2015-07-15 18:27:45 +0200

    Try running Riak.

--- travis-riak-weirdness.sh
+++ travis-riak-weirdness.sh
@@ -19,26 +19,29 @@ echo $RIAK_VERSION
 echo "Built:"
 $HOME/riak/riak-${RIAK_VERSION}/bin/riak version
 
+$HOME/riak/riak-${RIAK_VERSION}/bin/riak start
+sleep 30
+
 # riak-admin ring-status
 # riak-admin status
 
 # echo -e "\nResults:\n"
 
-# curl -XPUT -d 'data1' \
-#     -H "x-riak-index-field1_bin: val1" \
-#     'http://127.0.0.1:8098/riak/mybucket/foo%3Abar'
+curl -XPUT -d 'data1' \
+    -H "x-riak-index-field1_bin: val1" \
+    'http://127.0.0.1:8098/riak/mybucket/foo%3Abar'
 
-# curl -XPUT -d 'data2' \
-#     -H "x-riak-index-field1_bin: val2" \
-#     'http://127.0.0.1:8098/riak/mybucket/foo:baz'
+curl -XPUT -d 'data2' \
+    -H "x-riak-index-field1_bin: val2" \
+    'http://127.0.0.1:8098/riak/mybucket/foo:baz'
 
-# curl 'http://127.0.0.1:8098/riak/mybucket/foo%3Abar'; echo ""
-# curl 'http://127.0.0.1:8098/riak/mybucket/foo:bar'; echo ""
-# curl 'http://localhost:8098/buckets/mybucket/index/field1_bin/val1'; echo ""
+curl 'http://127.0.0.1:8098/riak/mybucket/foo%3Abar'; echo ""
+curl 'http://127.0.0.1:8098/riak/mybucket/foo:bar'; echo ""
+curl 'http://localhost:8098/buckets/mybucket/index/field1_bin/val1'; echo ""
 
-# curl 'http://127.0.0.1:8098/riak/mybucket/foo%3Abaz'; echo ""
-# curl 'http://127.0.0.1:8098/riak/mybucket/foo:baz'; echo ""
-# curl 'http://localhost:8098/buckets/mybucket/index/field1_bin/val2'; echo ""
+curl 'http://127.0.0.1:8098/riak/mybucket/foo%3Abaz'; echo ""
+curl 'http://127.0.0.1:8098/riak/mybucket/foo:baz'; echo ""
+curl 'http://localhost:8098/buckets/mybucket/index/field1_bin/val2'; echo ""
 
 # echo -e "\napp.config:\n"
 # cat /etc/riak/app.config
