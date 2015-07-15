e47892f9d36eed9ec0c115039c7a149e4c95f02a
Jeremy Thurgood <firxen@gmail.com>
2015-07-15 14:52:42 +0200

    Use our own Erlang.

--- riak-1.4.12-install.sh
+++ riak-1.4.12-install.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+curl -O https://raw.github.com/spawngrid/kerl/master/kerl
+chmod a+x kerl
+
+kerl build git git://github.com/basho/otp.git OTP_R16B02_basho8 R16B02-basho8
+
+ls ~/erlang
+
+./kerl install R16B02-basho8 ~/erlang/R16B02-basho8
+
+ls ~/erlang
+
+. ~/erlang/R16B02-basho8/activate
+
 wget http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.12/riak-1.4.12.tar.gz
 tar zxvf riak-1.4.12.tar.gz
 cd riak-1.4.12
--- riak-2.1.1-install.sh
+++ riak-2.1.1-install.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+curl -O https://raw.github.com/spawngrid/kerl/master/kerl
+chmod a+x kerl
+
+kerl build git git://github.com/basho/otp.git OTP_R16B02_basho8 R16B02-basho8
+
+ls ~/erlang
+
+./kerl install R16B02-basho8 ~/erlang/R16B02-basho8
+
+ls ~/erlang
+
+. ~/erlang/R16B02-basho8/activate
+
 
 wget http://s3.amazonaws.com/downloads.basho.com/riak/2.1/2.1.1/riak-2.1.1.tar.gz
 tar zxvf riak-2.1.1.tar.gz
