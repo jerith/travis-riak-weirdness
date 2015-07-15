fa036a831969c5be73f67c3ce5ebca0c770a6f9b
Jeremy Thurgood <firxen@gmail.com>
2015-07-15 14:41:43 +0200

    Install Riaks from source.

---
+++ riak-1.4.12-install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.12/riak-1.4.12.tar.gz
+tar zxvf riak-1.4.12.tar.gz
+cd riak-1.4.12
+make rel
---
+++ riak-2.1.1-install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget http://s3.amazonaws.com/downloads.basho.com/riak/2.1/2.1.1/riak-2.1.1.tar.gz
+tar zxvf riak-2.1.1.tar.gz
+cd riak-2.1.1
+make rel
