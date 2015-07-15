ad439cb96dc448593d42ec871de8191bf2b7c7be
Jeremy Thurgood <firxen@gmail.com>
2015-07-15 18:35:20 +0200

    More build stuff.

--- riak-1.4.12-install.sh
+++ riak-1.4.12-install.sh
@@ -4,7 +4,11 @@ ERL_BUILD=r15b01
 ./install-erlang.sh $ERL_BUILD R15B01
 . $HOME/erlang/$ERL_BUILD/activate
 
-if [ ! -d $HOME/riak/riak-1.4.12 ]; then
+RIAK_CACHE_DIR="$HOME/riak/riak-1.4.12"
+
+# Once-off to kill bad cache.
+rm -rf $RIAK_CACHE_DIR
+if [ ! -d $RIAK_CACHE_DIR ]; then
     wget http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.12/riak-1.4.12.tar.gz
     tar zxvf riak-1.4.12.tar.gz
     cd riak-1.4.12
@@ -12,7 +16,9 @@ if [ ! -d $HOME/riak/riak-1.4.12 ]; then
     cd ..
 
     mkdir -p $HOME/riak
-    cp -a riak-1.4.12/rel/riak $HOME/riak/riak-1.4.12
+    cp -a riak-1.4.12/rel/riak $RIAK_CACHE_DIR
 else
     echo "Cached build found, using that."
 fi
+
+cp -a $RIAK_CACHE_DIR riak
--- riak-2.1.1-install.sh
+++ riak-2.1.1-install.sh
@@ -4,7 +4,11 @@ ERL_BUILD=R16B02-basho8
 ./install-erlang.sh $ERL_BUILD git git://github.com/basho/otp.git OTP_R16B02_basho8
 . $HOME/erlang/$ERL_BUILD/activate
 
-if [ ! -d $HOME/riak/riak-2.1.1 ]; then
+RIAK_CACHE_DIR="$HOME/riak/riak-2.1.1"
+
+# Once-off to kill bad cache.
+rm -rf $RIAK_CACHE_DIR
+if [ ! $RIAK_CACHE_DIR ]; then
     wget http://s3.amazonaws.com/downloads.basho.com/riak/2.1/2.1.1/riak-2.1.1.tar.gz
     tar zxvf riak-2.1.1.tar.gz
     cd riak-2.1.1
@@ -13,7 +17,9 @@ if [ ! -d $HOME/riak/riak-2.1.1 ]; then
     cd ..
 
     mkdir -p $HOME/riak
-    cp -a riak-2.1.1/rel/riak $HOME/riak/riak-2.1.1
+    cp -a riak-2.1.1/rel/riak $RIAK_CACHE_DIR
 else
     echo "Cached build found, using that."
 fi
+
+cp -a $RIAK_CACHE_DIR riak
