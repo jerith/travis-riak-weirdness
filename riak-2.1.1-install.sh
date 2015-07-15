87798875a063cbcade3834cd29f42a53137d3500
Jeremy Thurgood <firxen@gmail.com>
2015-07-15 15:38:19 +0200

    No more Erlang building, instead look for version.

--- riak-2.1.1-install.sh
+++ riak-2.1.1-install.sh
@@ -1,19 +1,21 @@
 #!/bin/sh -x
 
-ls
-curl -O https://raw.githubusercontent.com/spawngrid/kerl/master/kerl
-ls
-chmod a+x kerl
+erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell
 
-./kerl build git git://github.com/basho/otp.git OTP_R16B02_basho8 R16B02-basho8
+# ls
+# curl -O https://raw.githubusercontent.com/spawngrid/kerl/master/kerl
+# ls
+# chmod a+x kerl
 
-ls ~/erlang
+# ./kerl build git git://github.com/basho/otp.git OTP_R16B02_basho8 R16B02-basho8
 
-./kerl install R16B02-basho8 ~/erlang/R16B02-basho8
+# ls ~/erlang
 
-ls ~/erlang
+# ./kerl install R16B02-basho8 ~/erlang/R16B02-basho8
 
-. ~/erlang/R16B02-basho8/activate
+# ls ~/erlang
+
+# . ~/erlang/R16B02-basho8/activate
 
 
 wget http://s3.amazonaws.com/downloads.basho.com/riak/2.1/2.1.1/riak-2.1.1.tar.gz
