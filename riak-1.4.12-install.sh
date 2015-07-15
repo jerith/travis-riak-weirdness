2d3b79e08ad46b57f8fd0a22df5b35af4bebaf12
Jeremy Thurgood <firxen@gmail.com>
2015-07-15 17:41:08 +0200

    Build Riak again.

--- riak-1.4.12-install.sh
+++ riak-1.4.12-install.sh
@@ -7,4 +7,4 @@ ERL_BUILD=r15b01
 wget http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.12/riak-1.4.12.tar.gz
 tar zxvf riak-1.4.12.tar.gz
 cd riak-1.4.12
-# make rel
+make rel
--- riak-2.1.1-install.sh
+++ riak-2.1.1-install.sh
@@ -7,4 +7,4 @@ ERL_BUILD=R16B02-basho8
 wget http://s3.amazonaws.com/downloads.basho.com/riak/2.1/2.1.1/riak-2.1.1.tar.gz
 tar zxvf riak-2.1.1.tar.gz
 cd riak-2.1.1
-# make rel
+make rel
