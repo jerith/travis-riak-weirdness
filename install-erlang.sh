28dd0a2f65d4cf3f21b5dc3246e34c9fe22e6bff
Jeremy Thurgood <firxen@gmail.com>
2015-07-15 16:08:36 +0200

    Wait for build log to exist.

--- install-erlang.sh
+++ install-erlang.sh
@@ -4,7 +4,12 @@ curl -O https://raw.githubusercontent.com/spawngrid/kerl/master/kerl
 chmod a+x kerl
 
 ./kerl build git git://github.com/basho/otp.git OTP_R16B02_basho8 R16B02-basho8 &
-tail -f ~/.kerl/builds/R16B02-basho8/otp_build.log &
+
+LOGFILE="$HOME/.kerl/builds/R16B02-basho8/otp_build.log"
+while [ ! -e $LOGFILE ]; do
+    sleep 1
+done
+tail -f $LOGFILE &
 fg %1
 jobs
 
